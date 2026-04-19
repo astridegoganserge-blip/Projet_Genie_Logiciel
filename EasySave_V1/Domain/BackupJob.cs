415eaba1bb645499c619b77148bbee27013aea51
Death@229 <Death@229@example.com>
2026-04-19 19:18:42 +0200

    Création classe BackupJob pour la source, la destination, le nom de la sauvegarde et le type de sauvegarde

---
+++ EasySave_V1/Domain/BackupJob.cs
@@ -0,0 +1,15 @@
+﻿namespace EasySave.Domain;
+
+public sealed class BackupJob
+{
+    public int Id { get; init; }
+    public string Name { get; init; } = string.Empty;
+    public string SourcePath { get; init; } = string.Empty;
+    public string DestinationPath { get; init; } = string.Empty;
+    public BackupType Type { get; init; }
+
+    public override string ToString()
+    {
+        return $"Id: {Id}, Name: {Name}, SourcePath: {SourcePath}, DestinationPath: {DestinationPath}, Type: {Type}";
+    }
+}
