79e0e4df1eaa6e7bbda8b7897157cc9e48638011
Death@229 <Death@229@example.com>
2026-04-21 10:52:59 +0200

    mise à jour

---
+++ EasySave_V1/Domain/FileEntry.cs
@@ -0,0 +1,8 @@
+﻿namespace EasySave.Domain;
+
+public sealed class FileEntry
+{
+    public string SourcePath { get; init; } = string.Empty;
+    public string TargetPath { get; init; } = string.Empty;
+    public long Size { get; init; }
+}
