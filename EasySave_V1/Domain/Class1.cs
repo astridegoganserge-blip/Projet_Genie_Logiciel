f8c31841b598a2217a847b5878bb826f41479723
Death@229 <Death@229@example.com>
2026-04-22 16:14:31 +0200

    domain enlever

--- EasySave_V1/Domain/Class1.cs
+++
@@ -1,1 +0,0 @@
-﻿using EasySave.Domain;
--- EasySave_V1/Domain/FileEntry.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EasySave.Domain;
-
-public sealed class FileEntry
-{
-    public string SourcePath { get; init; } = string.Empty;
-    public string TargetPath { get; init; } = string.Empty;
-    public long Size { get; init; }
-}
