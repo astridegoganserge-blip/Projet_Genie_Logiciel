e93912688b486f3b3d704459e3325b72068c121f
Death@229 <Death@229@example.com>
2026-05-03 19:28:25 +0200

    eat(v2): add core project and v2 models

---
+++ EasySave.Core/Models/JobState.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace EasySave.Core.Models
+{
+    public class JobState
+    {
+        public string BackupName { get; set; } = string.Empty;
+
+        public DateTime LastActionTime { get; set; }
+
+        public JobStatus Status { get; set; } = JobStatus.Terminé;
+
+        public int TotalFiles { get; set; }
+
+        public long TotalSize { get; set; }
+
+        public int RemainingFiles { get; set; }
+
+        public long RemainingSize { get; set; }
+
+        public double Progression { get; set; }
+
+        public string CurrentSourceFile { get; set; } = string.Empty;
+
+        public string CurrentTargetFile { get; set; } = string.Empty;
+    }
+}
