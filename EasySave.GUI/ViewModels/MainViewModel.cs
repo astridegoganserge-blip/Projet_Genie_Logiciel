ccace8889053287aa85b3daf5424ca3738829414
Death@229 <Death@229@example.com>
2026-05-03 16:36:06 +0200

    feat(v2): connect main window to main view model

---
+++ EasySave.GUI/ViewModels/MainViewModel.cs
@@ -0,0 +1,58 @@
+namespace EasySave.GUI.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private object? _currentView;
+        private string _applicationTitle = "EasySave v2.0";
+
+        public MainViewModel()
+        {
+            ShowJobsCommand = new RelayCommand(_ => ShowJobs());
+            ShowSettingsCommand = new RelayCommand(_ => ShowSettings());
+            ShowExecutionCommand = new RelayCommand(_ => ShowExecution());
+
+            CurrentView = "Jobs view will be implemented in the next step.";
+        }
+
+        public object? CurrentView
+        {
+            get => _currentView;
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string ApplicationTitle
+        {
+            get => _applicationTitle;
+            set
+            {
+                _applicationTitle = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public RelayCommand ShowJobsCommand { get; }
+
+        public RelayCommand ShowSettingsCommand { get; }
+
+        public RelayCommand ShowExecutionCommand { get; }
+
+        private void ShowJobs()
+        {
+            CurrentView = "Jobs view will be implemented in the next step.";
+        }
+
+        private void ShowSettings()
+        {
+            CurrentView = "Settings view will be implemented later.";
+        }
+
+        private void ShowExecution()
+        {
+            CurrentView = "Execution monitoring view will be implemented later.";
+        }
+    }
+}
